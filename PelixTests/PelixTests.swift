6de6ea1f72c119aa03c4842696a01a72a28f6bbe
diegotrejo <dietrej12@gmail.com>
2025-02-21 17:50:31 -0600

    Initial Commit

---
+++ PelixTests/PelixTests.swift
@@ -0,0 +1,17 @@
+//
+//  PelixTests.swift
+//  PelixTests
+//
+//  Created by Diego Trejo on 21/02/25.
+//
+
+import Testing
+@testable import Pelix
+
+struct PelixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
