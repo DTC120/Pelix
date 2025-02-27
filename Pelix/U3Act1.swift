b6e60277f7b1df82f9149eb2f28b6f8afb2f4645
diegotrejo <dietrej12@gmail.com>
2025-02-26 23:27:41 -0600

    Actividad 1

---
+++ Pelix/U3Act1.swift
@@ -0,0 +1,18 @@
+//
+//  U3Act1.swift
+//  Pelix
+//
+//  Created by Diego Trejo on 26/02/25.
+//
+
+import SwiftUI
+
+struct U3Act1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    U3Act1()
+}
